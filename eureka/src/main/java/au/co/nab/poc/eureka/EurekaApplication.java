d16f7f8a63e26bb4599f2b8e0c79cafc1602f091
tuan.tran <tuan.tran@metanet.co.kr>
2021-04-19 10:11:46 +0700

    Init eureka

---
+++ eureka/src/main/java/au/co/nab/poc/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package au.co.nab.poc.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author andy - tuantda.uit@gmail.com
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
